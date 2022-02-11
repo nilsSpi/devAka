549768209323c6720dab67bcd51a616b4a354eac
nils <nils.christian@web.de>
2022-02-11 11:50:45 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+
+function english(){
+    document.getElementById('str1').innerHTML=`Lern programming without any pre-exsisting knowledge`;
+    document.getElementById('str2').innerHTML=`We help you to become a succesfull programmer!`
+}
+
+function german(){
+    document.getElementById('str1').innerHTML=`Lernen Sie Programmieren ohne Vorkenntnisse `;
+    document.getElementById('str2').innerHTML=`Wir bilden Sie in 3-6 Monaten zu einem gefragtem Programmierer aus, der mit Leichtigkeit einen job findet`
+}
+
+function loremIpsum(){
+    document.getElementById('str1').innerHTML=`Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi, possimus?`;
+    document.getElementById('str2').innerHTML=`Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis repellat aliquid tenetur a voluptatem amet blanditiis placeat dicta ducimus vero?`;
+}
+
